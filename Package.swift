d687315158433a84dae5f611b4eb8acd8e2e74b9
David Idol <david.idol@gmail.com>
2024-01-25 13:12:40 -0500

    Make encoder/decoder configurable

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Disk",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "Disk", targets: ["Disk"])
     ],
